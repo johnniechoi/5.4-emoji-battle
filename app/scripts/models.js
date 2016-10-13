8f29c9d96a0147be39cff577bbb4b22062760d98
Gregory Shuford <bearshuford@me.com>
2016-10-13 16:47:26 -0400

    implemented constructor functions

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -1,10 +1,17 @@
 var $ = require('jQuery');
+var _ = require('jQuery');
 
 function Emoji(config) {
    config = config || {};
    $.extend(this, config);
+   this.health = config.health || 25;
+   this.content = config.content;
+}
+
+Emoji.prototype.lowerHealth = function() {
+   this.health -= _.random(2,6);
 }
 
 Emoji.prototype.attack = function(adversary /* Emoji */){
-   
-};
+   adversary.lowerHealth();
+}
