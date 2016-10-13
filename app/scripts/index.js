b23a2513adfcd0780e97f33afcfd39f1049cf2a1
Gregory Shuford <bearshuford@me.com>
2016-10-13 15:19:15 -0400

    outlined some application logic

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,2 +1,39 @@
 var $ = require('jQuery');
 var _ = require('underscore');
+var models = require('./models');
+
+
+
+// wait for DOM to be ready
+$(function(){
+
+
+  var player; // selected character
+
+  //////////////////////
+  // SELECT CHARACTER //
+  //////////////////////
+
+   // on 'start button' click assign selected character to player
+
+
+
+
+
+  //////////////////
+  // SELECT ENEMY //
+  //////////////////
+
+  // randomly select enemy
+
+
+
+  //////////////////
+  // SETUP BATTLE //
+  //////////////////
+
+
+
+
+
+});
