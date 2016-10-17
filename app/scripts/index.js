86ba1df699b2297eb4400f52fee37f2ee5cc88e6
Gregory Shuford <bearshuford@me.com>
2016-10-17 14:21:37 -0400

    fixed timing and cleaned up index.js

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,9 +3,6 @@ var _ = require('underscore');
 var emojione = require('emojione')
 var models = require('./models');
 
-// wait for DOM to be ready
-
-
 var good = [ new models.Emoji({power: 8, health: 25, code: ':grinning:'   }),
              new models.Emoji({power: 8, health: 25, code: ':older_woman:'}),
              new models.Emoji({power: 8, health: 25, code: ':santa:'      }),
@@ -19,32 +16,26 @@ var good = [ new models.Emoji({power: 8, health: 25, code: ':grinning:'   }),
              new models.Emoji({power: 8, health: 25, code: ':imp:'   })
            ];
 
-
-
-// '<div class="{name}-panel"></div>'
-var makePanel = function( $name) {
-   return;
-};
-
 $(function(){
 
    var $select = $('.overlay');
    var $attackButton = $('button')
 
-
-   $select.append('<div class="select-panel"></div>');
-   var $playerSelect = $('.select-panel');
-
    var selectedPlayer;
    var selectedEnemy;
    var $attackButton = $('button');
 
+   var gameOver = false;
+
+   $select.append('<div class="select-panel"></div>');
+   var $playerSelect = $('.select-panel');
 
    // fill selection panel
    _.each(good, function(emo){
-      var $image = $(emo.image);
-      $image.click(function(event) {
 
+      var $image = $(emo.image);
+
+      $image.click(function(event) {
          $(this).addClass("animated pulse").on("webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd", function(event) {
             $(this).removeClass("animated pulse");
             setTimeout(function(){
@@ -54,24 +45,19 @@ $(function(){
             selectedPlayer = emo;
             selectedEnemy  = _.sample(bad);
 
-
             $attackButton.html(emojione.toImage(':dagger:'));
-
 
             $('.player .image').html(selectedPlayer.image);
             $('.enemy .image').html(selectedEnemy.image);
 
             selectedPlayer.setHealthBar($('.player .healthBar'));
             selectedEnemy.setHealthBar($('.enemy .healthBar'));
-
          });
-
       });
 
       $image.mouseenter(function(event){
          $(this).find('use').addClass("animated bounce");
-      })
-
+      });
       $image.on("webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd", function(event) {
          $(this).find('use').removeClass("animated bounce");
      });
@@ -80,49 +66,47 @@ $(function(){
    });
 
 
-
-
    $attackButton.click(function(event){
       event.preventDefault();
+
       $attackButton.prop('disabled', true);
       $(this).addClass('animated swing').on("webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd", function(event) {
          $(this).removeClass("animated swing");
       });
-
 
       selectedPlayer.attack(selectedEnemy);
 
       setTimeout(function(){
          selectedEnemy.attack(selectedPlayer);
          setTimeout(function(){
-            $attackButton.prop('disabled', false);
+            if(!gameOver){ // keep disabled if game is over
+               $attackButton.prop('disabled', false);
+            }
          }, 800); // extra delay for button
       }, 1500);  // delay for enemy attack
    });
 
    $(document).on('emoji:death', function(event, deceased){
-      if(deceased === selectedPlayer) {
-         // console.log('Player died');
+      if(deceased === selectedPlayer) {   // player died
          $('.player svg').addClass('animated hinge')
             .on("webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd", function(event) {
                $('.player .image').html(emojione.toImage(':skull:'));
                $('.player svg').addClass('animated fadeIn');
                selectedPlayer.$healthBar.empty();
                $attackButton.prop('disabled', true);
-            }
-         );
-
+         });
       }
-      else {
-         // console.log('Enemy died');
+      else {   // enemy died
          $('.enemy svg').addClass('animated hinge')
             .on("webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd", function(event) {
                $('.enemy .image').html(emojione.toImage(':skull:'));
                $('.enemy svg').addClass('animated fadeIn');
                selectedEnemy.$healthBar.empty();
-               $attackButton.prop('disabled', true);
          });
      }
+
+     gameOver = true;
+     $attackButton.prop('disabled', true);
 
   });
 
